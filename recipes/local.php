b66224cb22ede53f975b0791b49a15b3c486a6c3
Andero Koplus <andero.koplus@gmail.com>
2016-09-12 16:26:38 +0300

    runLocally timeout

--- recipes/local.php
+++ recipes/local.php
@@ -10,17 +10,19 @@
  * Auto detectors have non-UNIX OS problems, so we are highly recommended
  * using your paths instead of it.
  */
+set('timeout', 60);
+
 env('local_bin/php', function () {
-    return runLocally('which php')->toString();
+    return runLocally('which php', get('timeout'))->toString();
 });
 env('local_bin/git', function () {
-    return runLocally('which git')->toString();
+    return runLocally('which git', get('timeout'))->toString();
 });
 env('local_bin/composer', function () {
-    $composer = runLocally('which composer')->toString();
+    $composer = runLocally('which composer', get('timeout'))->toString();
 
     if (empty($composer)) {
-        runLocally("cd {{release_path}} && curl -sS https://getcomposer.org/installer | {{local_bin/php}}");
+        runLocally("cd {{release_path}} && curl -sS https://getcomposer.org/installer | {{local_bin/php}}", get('timeout'));
         $composer = '{{local_bin/php}} {{local_release_path}}/composer.phar';
     }
 
@@ -33,7 +35,7 @@ env('local_bin/composer', function () {
  *  release workflow, that allows You to take advantage of this setting
  */
 env('local_git_cache', function() {
-    $gitVersion = runLocally('{{local_bin/git}} version');
+    $gitVersion = runLocally('{{local_bin/git}} version', get('timeout'));
     $regs = [];
     if (preg_match('/((\d+\.?)+)/', $gitVersion, $regs)) {
         $version = $regs[1];
@@ -50,7 +52,7 @@ env('local_deploy_path', '/tmp/deployer');
  * Return list of releases on server.
  */
 env('local_releases_list', function () {
-    $list = runLocally('ls {{local_deploy_path}}/releases')->toArray();
+    $list = runLocally('ls {{local_deploy_path}}/releases', get('timeout'))->toArray();
 
     rsort($list);
 
@@ -61,14 +63,14 @@ env('local_releases_list', function () {
  * Return release path.
  */
 env('local_release_path', function () {
-    return str_replace("\n", '', runLocally("readlink {{local_deploy_path}}/release"));
+    return str_replace("\n", '', runLocally("readlink {{local_deploy_path}}/release", get('timeout')));
 });
 
 /**
  * Return current release path.
  */
 env('local_current', function () {
-    return runLocally("readlink {{local_deploy_path}}/current")->toString();
+    return runLocally("readlink {{local_deploy_path}}/current", get('timeout'))->toString();
 });
 
 /**
@@ -76,15 +78,15 @@ env('local_current', function () {
  */
 task('local:prepare', function () {
 
-    runLocally('mkdir -p {{local_deploy_path}}'); //just to make sure everything exists
+    runLocally('mkdir -p {{local_deploy_path}}', get('timeout')); //just to make sure everything exists
 
-    runLocally('if [ ! -d {{local_deploy_path}} ]; then echo ""; fi');
+    runLocally('if [ ! -d {{local_deploy_path}} ]; then echo ""; fi', get('timeout'));
 
     // Create releases dir.
-    runLocally("cd {{local_deploy_path}} && if [ ! -d releases ]; then mkdir releases; fi");
+    runLocally("cd {{local_deploy_path}} && if [ ! -d releases ]; then mkdir releases; fi", get('timeout'));
 
     // Create shared dir.
-    runLocally("cd {{local_deploy_path}} && if [ ! -d shared ]; then mkdir shared; fi");
+    runLocally("cd {{local_deploy_path}} && if [ ! -d shared ]; then mkdir shared; fi", get('timeout'));
 })->desc('Preparing for local deploy');
 
 /**
@@ -100,11 +102,11 @@ task('local:release', function () {
         $releasePath .= '.' . ++$i;
     }
 
-    runLocally("mkdir -p $releasePath");
+    runLocally("mkdir -p $releasePath", get('timeout'));
 
-    runLocally("cd {{local_deploy_path}} && if [ -h release ]; then rm release; fi");
+    runLocally("cd {{local_deploy_path}} && if [ -h release ]; then rm release; fi", get('timeout'));
 
-    runLocally("ln -s $releasePath {{local_deploy_path}}/release");
+    runLocally("ln -s $releasePath {{local_deploy_path}}/release", get('timeout'));
 })->desc('Prepare local release');
 
 /**
@@ -132,14 +134,14 @@ task('local:update_code', function () {
 
     if ($gitCache && isset($releases[1])) {
         try {
-            runLocally("$git clone $at --recursive -q --reference {{local_deploy_path}}/releases/{$releases[1]} --dissociate $repository  {{local_release_path}} 2>&1");
+            runLocally("$git clone $at --recursive -q --reference {{local_deploy_path}}/releases/{$releases[1]} --dissociate $repository  {{local_release_path}} 2>&1", get('timeout'));
         } catch (\RuntimeException $e) {
             // If {{local_deploy_path}}/releases/{$releases[1]} has a failed git clone, is empty, shallow etc, git would throw error and give up. So we're forcing it to act without reference in this situation
-            runLocally("$git clone $at --recursive -q $repository {{local_release_path}} 2>&1");
+            runLocally("$git clone $at --recursive -q $repository {{local_release_path}} 2>&1", get('timeout'));
         }
     } else {
         // if we're using git cache this would be identical to above code in catch - full clone. If not, it would create shallow clone.
-        runLocally("$git clone $at $depth --recursive -q $repository {{local_release_path}} 2>&1");
+        runLocally("$git clone $at $depth --recursive -q $repository {{local_release_path}} 2>&1", get('timeout'));
     }
 })->desc('Updating code');
 
@@ -147,15 +149,15 @@ task('local:update_code', function () {
  * Installing vendors tasks.
  */
 task('local:vendors', function () {
-    runLocally("cd {{local_release_path}} && {{env_vars}} {{local_bin/composer}} {{composer_options}}");
+    runLocally("cd {{local_release_path}} && {{env_vars}} {{local_bin/composer}} {{composer_options}}", get('timeout'));
 })->desc('Installing vendors locally');
 
 /**
  * Create symlink to last release.
  */
 task('local:symlink', function () {
-    runLocally("cd {{local_deploy_path}} && ln -sfn {{local_release_path}} current"); // Atomic override symlink.
-    runLocally("cd {{local_deploy_path}} && rm release"); // Remove release link.
+    runLocally("cd {{local_deploy_path}} && ln -sfn {{local_release_path}} current", get('timeout')); // Atomic override symlink.
+    runLocally("cd {{local_deploy_path}} && rm release", get('timeout')); // Remove release link.
 })->desc('Creating symlink to local release');
 
 /**
@@ -179,9 +181,9 @@ task('local:cleanup', function () {
     }
 
     foreach ($releases as $release) {
-        runLocally("rm -rf {{local_deploy_path}}/releases/$release");
+        runLocally("rm -rf {{local_deploy_path}}/releases/$release", get('timeout'));
     }
 
-    runLocally("cd {{local_deploy_path}} && if [ -e release ]; then rm release; fi");
-    runLocally("cd {{local_deploy_path}} && if [ -h release ]; then rm release; fi");
+    runLocally("cd {{local_deploy_path}} && if [ -e release ]; then rm release; fi", get('timeout'));
+    runLocally("cd {{local_deploy_path}} && if [ -h release ]; then rm release; fi", get('timeout'));
 })->desc('Cleaning up old local releases');
